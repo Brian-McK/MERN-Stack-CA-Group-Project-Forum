025122aee47ebfaebaf09cd2401d6606f89767ff
desmondmad <73113678+desmondmad@users.noreply.github.com>
2021-03-02 10:04:39 +0000

    Changed to a class component

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,55 +1,82 @@
-import React, { useState } from 'react';
+
+import React, { Component} from 'react';
 import { Grid, Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faSignInAlt, faKey } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faSignInAlt, faKey, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {SERVER_HOST} from "../config/global_constants"
+import axios from "axios"
+import {Redirect, Link} from "react-router-dom"
+import LinkInClass from "../components/LinkInClass"
 
-const useStyles = makeStyles((theme) => ({
-	grid: {
-		width: '100%',
-		margin: '0 auto'
-	},
-	paper: {
-		padding: theme.spacing(1),
-		textAlign: 'center'
-	}
-}));
 
-export default function LoginForm () {
-	const [ email, setEmail ] = useState();
-	const [ password, setPassword ] = useState();
+export default class LoginForm extends Component {
+  constructor(props) {
+    super(props);
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		alert(`Email: ${email} , Password: ${password}`);
-	};
+    this.state = {
+      email: "",
+      password: "",
+      isLoggedIn:false
+    }
+  }
+  
+    handleChange = (e) => 
+    {
+        this.setState({[e.target.name]: e.target.value})
+    }
+    
+    handleSubmit = (e) =>
+    {
+        axios.post(`${SERVER_HOST}/users/login/${this.state.email}/${this.state.password}`)
+                .then(res => 
+                {     
+                    if(res.data)
+                    {
+                        if (res.data.errorMessage)
+                        {
+                            console.log(res.data.errorMessage)    
+                        }
+                        else // user successfully logged in
+                        { 
+                            console.log("User logged in")
+                            this.setState({isLoggedIn: true})
+                        }        
+                    }
+                    else
+                    {
+                        console.log("Login failed")
+                    }
+                })  
+    }
+    
 
-	const classes = useStyles();
-
-	return (
+    render(){
+      return (
 		<div className="LoginForm">
 			This is My Login Form Component
-			<Grid container spacing={2} className={classes.grid}>
+			<Grid container spacing={2}>
 				<Grid item xs={12} sm={12} md={4} lg={4} className="LoginFormInfoWrapper">
 					<h3>Hello & Welcome</h3>
 					<FontAwesomeIcon icon={faSignInAlt} size="4x" />
 				</Grid>
 				<Grid item xs={12} sm={12} md={8} lg={8} className="LoginFormWrapper">
-					<Paper square className={classes.paper} elevation={5}>
+					<Paper square elevation={5}>
 						<h3>Please Log In</h3>
-						<form onSubmit={handleSubmit}>
+						<form onSubmit={this.handleSubmit}>
 							<label className="FormLabels">
 								<FontAwesomeIcon icon={faEnvelope} size="2x" />
 								<p>Email</p>
-								<input type="email" onChange={(e) => setEmail(e.target.value)} />
+								<input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
 							</label>
 							<label className="FormLabels">
 								<FontAwesomeIcon icon={faKey} size="2x" />
 								<p>Password</p>
-								<input type="password" onChange={(e) => setPassword(e.target.value)} />
+								<input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
 							</label>
 							<div>
-								<button type="submit">Submit</button>
+								<LinkInClass value="User Login" className="green-button" onClick={this.handleSubmit} />
+                                                                {this.state.isLoggedIn ? <Redirect to="/"/> : null} 
+                                                                <Link className="red-button" to={"/"}>Cancel</Link>  
 							</div>
 						</form>
 					</Paper>
@@ -57,4 +84,7 @@ export default function LoginForm () {
 			</Grid>
 		</div>
 	);
+  }
 }
+
+
